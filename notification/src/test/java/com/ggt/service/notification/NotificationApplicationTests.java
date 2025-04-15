3dc1b67780774fc8055d84e0b9a6853719fb1f87
akshay <assistant5@globalgreentag.com>
2025-04-16 09:47:26 +1000

    Initial Commit for notification service (Derry agreed to have a meeting)

---
+++ notification/src/test/java/com/ggt/service/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggt.service.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
